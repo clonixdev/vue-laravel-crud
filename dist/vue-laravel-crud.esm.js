27acb0e0453a029a67c15eb3a030485bf01a02fd
Maximiliano Zogbi <maxizrnr@gmail.com>
2023-12-22 04:48:35 -0300

    Fixes

--- dist/vue-laravel-crud.esm.js
+++ dist/vue-laravel-crud.esm.js
@@ -11878,7 +11878,7 @@ var InfiniteLoading = /*@__PURE__*/getDefaultExportFromCjs(vueInfiniteLoadingExp
 
 var e=[],t=[];function n(n,r){if(n&&"undefined"!=typeof document){var a,s=!0===r.prepend?"prepend":"append",d=!0===r.singleTag,i="string"==typeof r.container?document.querySelector(r.container):document.getElementsByTagName("head")[0];if(d){var u=e.indexOf(i);-1===u&&(u=e.push(i)-1,t[u]={}),a=t[u]&&t[u][s]?t[u][s]:t[u][s]=c();}else a=c();65279===n.charCodeAt(0)&&(n=n.substring(1)),a.styleSheet?a.styleSheet.cssText+=n:a.appendChild(document.createTextNode(n));}function c(){var e=document.createElement("style");if(e.setAttribute("type","text/css"),r.attributes)for(var t=Object.keys(r.attributes),n=0;n<t.length;n++)e.setAttribute(t[n],r.attributes[t[n]]);var a="prepend"===s?"afterbegin":"beforeend";return i.insertAdjacentElement(a,e),e}}
 
-var css = "tr td[data-v-a63589d7]:last-child,\ntr td[data-v-a63589d7]:first-child {\n  width: 1%;\n  white-space: nowrap; }\n\n.crud-pagination[data-v-a63589d7] {\n  display: flex;\n  justify-content: center; }\n\n.crud-header[data-v-a63589d7] {\n  display: flex;\n  justify-content: space-between;\n  max-height: 3rem; }\n  .crud-header[data-v-a63589d7] .crud-title[data-v-a63589d7] {\n    margin: 0; }\n  .crud-header[data-v-a63589d7] .crud-search[data-v-a63589d7] {\n    max-width: 15rem; }\n    .crud-header[data-v-a63589d7] .crud-search[data-v-a63589d7] .btn[data-v-a63589d7] {\n      border-top-left-radius: 0;\n      border-bottom-left-radius: 0;\n      border-top-right-radius: 0.375rem;\n      border-bottom-right-radius: 0.375rem; }\n      .crud-header[data-v-a63589d7] .crud-search[data-v-a63589d7] .btn[data-v-a63589d7].open[data-v-a63589d7] {\n        border-top-right-radius: 0;\n        border-bottom-right-radius: 0; }\n  .crud-header[data-v-a63589d7] .table-options[data-v-a63589d7] {\n    margin-bottom: 1rem;\n    display: flex;\n    align-items: center;\n    justify-content: flex-end; }\n\n.custom-control[data-v-a63589d7] {\n  position: relative;\n  top: -15px; }\n\n@media (min-width: 992px) {\n  .table[data-v-a63589d7] {\n    table-layout: auto; }\n    .table[data-v-a63589d7] tbody[data-v-a63589d7] td[data-v-a63589d7] {\n      overflow: scroll;\n      -ms-overflow-style: none;\n      /* IE and Edge */\n      scrollbar-width: none;\n      /* Firefox */ }\n    .table[data-v-a63589d7] tbody[data-v-a63589d7] td[data-v-a63589d7]::-webkit-scrollbar {\n      display: none; } }\n";
+var css = "tr td[data-v-d1f59823]:last-child,\ntr td[data-v-d1f59823]:first-child {\n  width: 1%;\n  white-space: nowrap; }\n\n.crud-pagination[data-v-d1f59823] {\n  display: flex;\n  justify-content: center; }\n\n.crud-header[data-v-d1f59823] {\n  display: flex;\n  justify-content: space-between;\n  max-height: 3rem; }\n  .crud-header[data-v-d1f59823] .crud-title[data-v-d1f59823] {\n    margin: 0; }\n  .crud-header[data-v-d1f59823] .crud-search[data-v-d1f59823] {\n    max-width: 15rem; }\n    .crud-header[data-v-d1f59823] .crud-search[data-v-d1f59823] .btn[data-v-d1f59823] {\n      border-top-left-radius: 0;\n      border-bottom-left-radius: 0;\n      border-top-right-radius: 0.375rem;\n      border-bottom-right-radius: 0.375rem; }\n      .crud-header[data-v-d1f59823] .crud-search[data-v-d1f59823] .btn[data-v-d1f59823].open[data-v-d1f59823] {\n        border-top-right-radius: 0;\n        border-bottom-right-radius: 0; }\n  .crud-header[data-v-d1f59823] .table-options[data-v-d1f59823] {\n    margin-bottom: 1rem;\n    display: flex;\n    align-items: center;\n    justify-content: flex-end; }\n\n.custom-control[data-v-d1f59823] {\n  position: relative;\n  top: -15px; }\n\n@media (min-width: 992px) {\n  .table[data-v-d1f59823] {\n    table-layout: auto; }\n    .table[data-v-d1f59823] tbody[data-v-d1f59823] td[data-v-d1f59823] {\n      overflow: scroll;\n      -ms-overflow-style: none;\n      /* IE and Edge */\n      scrollbar-width: none;\n      /* Firefox */ }\n    .table[data-v-d1f59823] tbody[data-v-d1f59823] td[data-v-d1f59823]::-webkit-scrollbar {\n      display: none; } }\n";
 n(css, {});
 
 function normalizeComponent (
@@ -12012,7 +12012,8 @@ const _sfc_main = {
       },
       infiniteScrollKey: 1,
       optionsLoaded: false,
-      isMobile: false
+      isMobile: false,
+      refreshing: false
     };
   },
   watch: {
@@ -12467,12 +12468,15 @@ const _sfc_main = {
       }
     },
     resetFilters() {
+      let refresh = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : true;
       this.internalFilters = [];
       this.setupFilters();
       this.forceRecomputeCounter++;
-      setTimeout(() => {
-        this.refresh();
-      }, 1);
+      if (refresh) {
+        setTimeout(() => {
+          this.refresh();
+        }, 1);
+      }
     },
     toggleDisplayMode() {
       if (this.displayMode == this.displayModes.MODE_TABLE) this.displayMode = this.displayModes.MODE_CARDS;else if (this.displayMode == this.displayModes.MODE_CARDS) this.displayMode = this.displayModes.MODE_TABLE;
@@ -12585,6 +12589,7 @@ const _sfc_main = {
       }
       const fetchPromise = this.fetchItems(this.pagination.current_page);
       if (this.infiniteScroll && fetchPromise) {
+        this.refreshing = true;
         fetchPromise.then(() => {
           const infiniteLoadingRef = this.$refs.infiniteLoading;
           if (infiniteLoadingRef) {
@@ -12592,6 +12597,7 @@ const _sfc_main = {
           } else {
             console.debug("infiniteLoadingRef not set");
           }
+          this.refreshing = false;
         });
       }
     },
@@ -14004,7 +14010,7 @@ var _sfc_render = function render() {
   }) : _vm._e()], 2)], 1);
 };
 var _sfc_staticRenderFns = [];
-var __component__ = /*#__PURE__*/normalizeComponent(_sfc_main, _sfc_render, _sfc_staticRenderFns, false, null, "a63589d7", null, null);
+var __component__ = /*#__PURE__*/normalizeComponent(_sfc_main, _sfc_render, _sfc_staticRenderFns, false, null, "d1f59823", null, null);
 var component = __component__.exports;
 
 // Import vue component
