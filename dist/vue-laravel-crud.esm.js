72e92939b996fb0eeb6c2ea5cc7c575ca13a20db
Maximiliano Zogbi <maxizrnr@gmail.com>
2024-06-23 02:34:34 -0300

    Vuex localforge create

--- dist/vue-laravel-crud.esm.js
+++ dist/vue-laravel-crud.esm.js
@@ -12115,7 +12115,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 
 var e=[],t=[];function n(n,r){if(n&&"undefined"!=typeof document){var a,s=!0===r.prepend?"prepend":"append",d=!0===r.singleTag,i="string"==typeof r.container?document.querySelector(r.container):document.getElementsByTagName("head")[0];if(d){var u=e.indexOf(i);-1===u&&(u=e.push(i)-1,t[u]={}),a=t[u]&&t[u][s]?t[u][s]:t[u][s]=c();}else a=c();65279===n.charCodeAt(0)&&(n=n.substring(1)),a.styleSheet?a.styleSheet.cssText+=n:a.appendChild(document.createTextNode(n));}function c(){var e=document.createElement("style");if(e.setAttribute("type","text/css"),r.attributes)for(var t=Object.keys(r.attributes),n=0;n<t.length;n++)e.setAttribute(t[n],r.attributes[t[n]]);var a="prepend"===s?"afterbegin":"beforeend";return i.insertAdjacentElement(a,e),e}}
 
-var css = "tr td[data-v-53ba538f]:last-child,\ntr td[data-v-53ba538f]:first-child {\n  width: 1%;\n  white-space: nowrap; }\n\n.crud-pagination[data-v-53ba538f] {\n  display: flex;\n  align-items: center;\n  width: 100%;\n  justify-content: center;\n  margin-top: 1rem; }\n\n.crud-header[data-v-53ba538f] {\n  display: flex;\n  justify-content: space-between;\n  max-height: 3rem; }\n  .crud-header[data-v-53ba538f] .crud-title[data-v-53ba538f] {\n    margin: 0; }\n  .crud-header[data-v-53ba538f] .crud-search[data-v-53ba538f] {\n    max-width: 15rem; }\n    .crud-header[data-v-53ba538f] .crud-search[data-v-53ba538f] .btn[data-v-53ba538f] {\n      border-top-left-radius: 0;\n      border-bottom-left-radius: 0;\n      border-top-right-radius: 0.375rem;\n      border-bottom-right-radius: 0.375rem; }\n      .crud-header[data-v-53ba538f] .crud-search[data-v-53ba538f] .btn[data-v-53ba538f].open[data-v-53ba538f] {\n        border-top-right-radius: 0;\n        border-bottom-right-radius: 0; }\n  .crud-header[data-v-53ba538f] .table-options[data-v-53ba538f] {\n    margin-bottom: 1rem;\n    display: flex;\n    align-items: center;\n    justify-content: flex-end; }\n\n.custom-control[data-v-53ba538f] {\n  position: relative; }\n\n@media (min-width: 992px) {\n  .table[data-v-53ba538f] {\n    table-layout: auto; }\n    .table[data-v-53ba538f] tbody[data-v-53ba538f] td[data-v-53ba538f] {\n      overflow: scroll;\n      -ms-overflow-style: none;\n      /* IE and Edge */\n      scrollbar-width: none;\n      /* Firefox */ }\n    .table[data-v-53ba538f] tbody[data-v-53ba538f] td[data-v-53ba538f]::-webkit-scrollbar {\n      display: none; } }\n";
+var css = "tr td[data-v-f26a8ffd]:last-child,\ntr td[data-v-f26a8ffd]:first-child {\n  width: 1%;\n  white-space: nowrap; }\n\n.crud-pagination[data-v-f26a8ffd] {\n  display: flex;\n  align-items: center;\n  width: 100%;\n  justify-content: center;\n  margin-top: 1rem; }\n\n.crud-header[data-v-f26a8ffd] {\n  display: flex;\n  justify-content: space-between;\n  max-height: 3rem; }\n  .crud-header[data-v-f26a8ffd] .crud-title[data-v-f26a8ffd] {\n    margin: 0; }\n  .crud-header[data-v-f26a8ffd] .crud-search[data-v-f26a8ffd] {\n    max-width: 15rem; }\n    .crud-header[data-v-f26a8ffd] .crud-search[data-v-f26a8ffd] .btn[data-v-f26a8ffd] {\n      border-top-left-radius: 0;\n      border-bottom-left-radius: 0;\n      border-top-right-radius: 0.375rem;\n      border-bottom-right-radius: 0.375rem; }\n      .crud-header[data-v-f26a8ffd] .crud-search[data-v-f26a8ffd] .btn[data-v-f26a8ffd].open[data-v-f26a8ffd] {\n        border-top-right-radius: 0;\n        border-bottom-right-radius: 0; }\n  .crud-header[data-v-f26a8ffd] .table-options[data-v-f26a8ffd] {\n    margin-bottom: 1rem;\n    display: flex;\n    align-items: center;\n    justify-content: flex-end; }\n\n.custom-control[data-v-f26a8ffd] {\n  position: relative; }\n\n@media (min-width: 992px) {\n  .table[data-v-f26a8ffd] {\n    table-layout: auto; }\n    .table[data-v-f26a8ffd] tbody[data-v-f26a8ffd] td[data-v-f26a8ffd] {\n      overflow: scroll;\n      -ms-overflow-style: none;\n      /* IE and Edge */\n      scrollbar-width: none;\n      /* Firefox */ }\n    .table[data-v-f26a8ffd] tbody[data-v-f26a8ffd] td[data-v-f26a8ffd]::-webkit-scrollbar {\n      display: none; } }\n";
 n(css, {});
 
 function normalizeComponent (
@@ -13285,7 +13285,6 @@ const _sfc_main = {
       let result;
       let create = false;
       if (this.vuexLocalforage) {
-        await this.model.$create(id);
         if (this.item.id) {
           result = await this.model.$update(this.item.id, jsondata);
           create = false;
@@ -14598,7 +14597,7 @@ var _sfc_render = function render() {
   }) : _vm._e()], 2) : _vm._e()], 1);
 };
 var _sfc_staticRenderFns = [];
-var __component__ = /*#__PURE__*/normalizeComponent(_sfc_main, _sfc_render, _sfc_staticRenderFns, false, null, "53ba538f", null, null);
+var __component__ = /*#__PURE__*/normalizeComponent(_sfc_main, _sfc_render, _sfc_staticRenderFns, false, null, "f26a8ffd", null, null);
 var component = __component__.exports;
 
 // Import vue component
--- dist/vue-laravel-crud.ssr.js
+++ dist/vue-laravel-crud.ssr.js
@@ -12532,7 +12532,7 @@ Plugin.install = function (Vue, options) {
 
 if (typeof window !== 'undefined' && window.Vue) {
   window.Vue.use(Plugin);
-}var e=[],t=[];function n(n,r){if(n&&"undefined"!=typeof document){var a,s=!0===r.prepend?"prepend":"append",d=!0===r.singleTag,i="string"==typeof r.container?document.querySelector(r.container):document.getElementsByTagName("head")[0];if(d){var u=e.indexOf(i);-1===u&&(u=e.push(i)-1,t[u]={}),a=t[u]&&t[u][s]?t[u][s]:t[u][s]=c();}else a=c();65279===n.charCodeAt(0)&&(n=n.substring(1)),a.styleSheet?a.styleSheet.cssText+=n:a.appendChild(document.createTextNode(n));}function c(){var e=document.createElement("style");if(e.setAttribute("type","text/css"),r.attributes)for(var t=Object.keys(r.attributes),n=0;n<t.length;n++)e.setAttribute(t[n],r.attributes[t[n]]);var a="prepend"===s?"afterbegin":"beforeend";return i.insertAdjacentElement(a,e),e}}var css = "tr td[data-v-53ba538f]:last-child,\ntr td[data-v-53ba538f]:first-child {\n  width: 1%;\n  white-space: nowrap; }\n\n.crud-pagination[data-v-53ba538f] {\n  display: flex;\n  align-items: center;\n  width: 100%;\n  justify-content: center;\n  margin-top: 1rem; }\n\n.crud-header[data-v-53ba538f] {\n  display: flex;\n  justify-content: space-between;\n  max-height: 3rem; }\n  .crud-header[data-v-53ba538f] .crud-title[data-v-53ba538f] {\n    margin: 0; }\n  .crud-header[data-v-53ba538f] .crud-search[data-v-53ba538f] {\n    max-width: 15rem; }\n    .crud-header[data-v-53ba538f] .crud-search[data-v-53ba538f] .btn[data-v-53ba538f] {\n      border-top-left-radius: 0;\n      border-bottom-left-radius: 0;\n      border-top-right-radius: 0.375rem;\n      border-bottom-right-radius: 0.375rem; }\n      .crud-header[data-v-53ba538f] .crud-search[data-v-53ba538f] .btn[data-v-53ba538f].open[data-v-53ba538f] {\n        border-top-right-radius: 0;\n        border-bottom-right-radius: 0; }\n  .crud-header[data-v-53ba538f] .table-options[data-v-53ba538f] {\n    margin-bottom: 1rem;\n    display: flex;\n    align-items: center;\n    justify-content: flex-end; }\n\n.custom-control[data-v-53ba538f] {\n  position: relative; }\n\n@media (min-width: 992px) {\n  .table[data-v-53ba538f] {\n    table-layout: auto; }\n    .table[data-v-53ba538f] tbody[data-v-53ba538f] td[data-v-53ba538f] {\n      overflow: scroll;\n      -ms-overflow-style: none;\n      /* IE and Edge */\n      scrollbar-width: none;\n      /* Firefox */ }\n    .table[data-v-53ba538f] tbody[data-v-53ba538f] td[data-v-53ba538f]::-webkit-scrollbar {\n      display: none; } }\n";
+}var e=[],t=[];function n(n,r){if(n&&"undefined"!=typeof document){var a,s=!0===r.prepend?"prepend":"append",d=!0===r.singleTag,i="string"==typeof r.container?document.querySelector(r.container):document.getElementsByTagName("head")[0];if(d){var u=e.indexOf(i);-1===u&&(u=e.push(i)-1,t[u]={}),a=t[u]&&t[u][s]?t[u][s]:t[u][s]=c();}else a=c();65279===n.charCodeAt(0)&&(n=n.substring(1)),a.styleSheet?a.styleSheet.cssText+=n:a.appendChild(document.createTextNode(n));}function c(){var e=document.createElement("style");if(e.setAttribute("type","text/css"),r.attributes)for(var t=Object.keys(r.attributes),n=0;n<t.length;n++)e.setAttribute(t[n],r.attributes[t[n]]);var a="prepend"===s?"afterbegin":"beforeend";return i.insertAdjacentElement(a,e),e}}var css = "tr td[data-v-f26a8ffd]:last-child,\ntr td[data-v-f26a8ffd]:first-child {\n  width: 1%;\n  white-space: nowrap; }\n\n.crud-pagination[data-v-f26a8ffd] {\n  display: flex;\n  align-items: center;\n  width: 100%;\n  justify-content: center;\n  margin-top: 1rem; }\n\n.crud-header[data-v-f26a8ffd] {\n  display: flex;\n  justify-content: space-between;\n  max-height: 3rem; }\n  .crud-header[data-v-f26a8ffd] .crud-title[data-v-f26a8ffd] {\n    margin: 0; }\n  .crud-header[data-v-f26a8ffd] .crud-search[data-v-f26a8ffd] {\n    max-width: 15rem; }\n    .crud-header[data-v-f26a8ffd] .crud-search[data-v-f26a8ffd] .btn[data-v-f26a8ffd] {\n      border-top-left-radius: 0;\n      border-bottom-left-radius: 0;\n      border-top-right-radius: 0.375rem;\n      border-bottom-right-radius: 0.375rem; }\n      .crud-header[data-v-f26a8ffd] .crud-search[data-v-f26a8ffd] .btn[data-v-f26a8ffd].open[data-v-f26a8ffd] {\n        border-top-right-radius: 0;\n        border-bottom-right-radius: 0; }\n  .crud-header[data-v-f26a8ffd] .table-options[data-v-f26a8ffd] {\n    margin-bottom: 1rem;\n    display: flex;\n    align-items: center;\n    justify-content: flex-end; }\n\n.custom-control[data-v-f26a8ffd] {\n  position: relative; }\n\n@media (min-width: 992px) {\n  .table[data-v-f26a8ffd] {\n    table-layout: auto; }\n    .table[data-v-f26a8ffd] tbody[data-v-f26a8ffd] td[data-v-f26a8ffd] {\n      overflow: scroll;\n      -ms-overflow-style: none;\n      /* IE and Edge */\n      scrollbar-width: none;\n      /* Firefox */ }\n    .table[data-v-f26a8ffd] tbody[data-v-f26a8ffd] td[data-v-f26a8ffd]::-webkit-scrollbar {\n      display: none; } }\n";
 n(css, {});function normalizeComponent (
     scriptExports,
     render,
@@ -13848,69 +13848,66 @@ n(css, {});function normalizeComponent (
               console.debug("save item 2", _this23.item, jsondata);
               create = false;
               if (!_this23.vuexLocalforage) {
-                _context6.next = 21;
+                _context6.next = 19;
                 break;
               }
-              _context6.next = 8;
-              return _this23.model.$create(id);
-            case 8:
               if (!_this23.item.id) {
-                _context6.next = 15;
+                _context6.next = 13;
                 break;
               }
-              _context6.next = 11;
+              _context6.next = 9;
               return _this23.model.$update(_this23.item.id, jsondata);
-            case 11:
+            case 9:
               result = _context6.sent;
               create = false;
-              _context6.next = 19;
-              break;
-            case 15:
               _context6.next = 17;
+              break;
+            case 13:
+              _context6.next = 15;
               return _this23.model.$create(jsondata);
-            case 17:
+            case 15:
               result = _context6.sent;
               create = true;
-            case 19:
-              _context6.next = 38;
+            case 17:
+              _context6.next = 36;
               break;
-            case 21:
+            case 19:
               if (!_this23.item.id) {
-                _context6.next = 28;
+                _context6.next = 26;
                 break;
               }
-              _context6.next = 24;
+              _context6.next = 22;
               return _this23.model.api().put('/' + _this23.item.id, jsondata);
-            case 24:
+            case 22:
               result = _context6.sent;
               create = false;
-              _context6.next = 32;
-              break;
-            case 28:
               _context6.next = 30;
+              break;
+            case 26:
+              _context6.next = 28;
               return _this23.model.api().post('', jsondata);
-            case 30:
+            case 28:
               result = _context6.sent;
               create = true;
-            case 32:
+            case 30:
               result.response.status;
               if (!result.response.data.error) {
-                _context6.next = 37;
+                _context6.next = 35;
                 break;
               }
               _this23.toastError(result.response.data.error);
               _this23.loading = false;
               return _context6.abrupt("return");
-            case 37:
+            case 35:
               result.save();
-            case 38:
+            case 36:
               if (_this23.refreshAfterSave) _this23.refresh();
               _this23.loading = false;
               _this23.toastSuccess("Elemento Modificado");
               if (_this23.hideModalAfterSave || create && _this23.hideModalAfterCreate || !create && _this23.hideModalAfterUpdate) {
                 _this23.$bvModal.hide("modal-form-item-" + _this23.modelName);
               }
-            case 42:
+            case 40:
             case "end":
               return _context6.stop();
           }
@@ -15274,7 +15271,7 @@ var _sfc_render = function render() {
   }) : _vm._e()], 2) : _vm._e()], 1);
 };
 var _sfc_staticRenderFns = [];
-var __component__ = /*#__PURE__*/normalizeComponent(_sfc_main, _sfc_render, _sfc_staticRenderFns, false, null, "53ba538f", null, null);
+var __component__ = /*#__PURE__*/normalizeComponent(_sfc_main, _sfc_render, _sfc_staticRenderFns, false, null, "f26a8ffd", null, null);
 var component$1 = __component__.exports;// Import vue component
 
 // Default export is installable instance of component.
