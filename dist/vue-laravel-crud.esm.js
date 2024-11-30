60e788ca126631c2f026bc8ab0592705ecae42b0
Maximiliano Zogbi <git@clonix.com.ar>
2024-11-29 22:23:13 -0300

    Version

--- dist/vue-laravel-crud.esm.js
+++ dist/vue-laravel-crud.esm.js
@@ -12115,7 +12115,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 
 var e=[],t=[];function n(n,r){if(n&&"undefined"!=typeof document){var a,s=!0===r.prepend?"prepend":"append",d=!0===r.singleTag,i="string"==typeof r.container?document.querySelector(r.container):document.getElementsByTagName("head")[0];if(d){var u=e.indexOf(i);-1===u&&(u=e.push(i)-1,t[u]={}),a=t[u]&&t[u][s]?t[u][s]:t[u][s]=c();}else a=c();65279===n.charCodeAt(0)&&(n=n.substring(1)),a.styleSheet?a.styleSheet.cssText+=n:a.appendChild(document.createTextNode(n));}function c(){var e=document.createElement("style");if(e.setAttribute("type","text/css"),r.attributes)for(var t=Object.keys(r.attributes),n=0;n<t.length;n++)e.setAttribute(t[n],r.attributes[t[n]]);var a="prepend"===s?"afterbegin":"beforeend";return i.insertAdjacentElement(a,e),e}}
 
-var css = "tr td[data-v-3ee88914]:last-child,\ntr td[data-v-3ee88914]:first-child {\n  width: 1%;\n  white-space: nowrap; }\n\n.crud-pagination[data-v-3ee88914] {\n  display: flex;\n  align-items: center;\n  width: 100%;\n  justify-content: center;\n  margin-top: 1rem; }\n\n.crud-header[data-v-3ee88914] {\n  display: flex;\n  justify-content: space-between;\n  max-height: 3rem; }\n  .crud-header[data-v-3ee88914] .crud-title[data-v-3ee88914] {\n    margin: 0; }\n  .crud-header[data-v-3ee88914] .crud-search[data-v-3ee88914] {\n    max-width: 15rem; }\n    .crud-header[data-v-3ee88914] .crud-search[data-v-3ee88914] .btn[data-v-3ee88914] {\n      border-top-left-radius: 0;\n      border-bottom-left-radius: 0;\n      border-top-right-radius: 0.375rem;\n      border-bottom-right-radius: 0.375rem; }\n      .crud-header[data-v-3ee88914] .crud-search[data-v-3ee88914] .btn[data-v-3ee88914].open[data-v-3ee88914] {\n        border-top-right-radius: 0;\n        border-bottom-right-radius: 0; }\n  .crud-header[data-v-3ee88914] .table-options[data-v-3ee88914] {\n    margin-bottom: 1rem;\n    display: flex;\n    align-items: center;\n    justify-content: flex-end; }\n\n.custom-control[data-v-3ee88914] {\n  position: relative; }\n\n@media (min-width: 992px) {\n  .table[data-v-3ee88914] {\n    table-layout: auto; }\n    .table[data-v-3ee88914] tbody[data-v-3ee88914] td[data-v-3ee88914] {\n      overflow: scroll;\n      -ms-overflow-style: none;\n      /* IE and Edge */\n      scrollbar-width: none;\n      /* Firefox */ }\n    .table[data-v-3ee88914] tbody[data-v-3ee88914] td[data-v-3ee88914]::-webkit-scrollbar {\n      display: none; } }\n";
+var css = "tr td[data-v-e6d5254f]:last-child,\ntr td[data-v-e6d5254f]:first-child {\n  width: 1%;\n  white-space: nowrap; }\n\n.crud-pagination[data-v-e6d5254f] {\n  display: flex;\n  align-items: center;\n  width: 100%;\n  justify-content: center;\n  margin-top: 1rem; }\n\n.crud-header[data-v-e6d5254f] {\n  display: flex;\n  justify-content: space-between;\n  max-height: 3rem; }\n  .crud-header[data-v-e6d5254f] .crud-title[data-v-e6d5254f] {\n    margin: 0; }\n  .crud-header[data-v-e6d5254f] .crud-search[data-v-e6d5254f] {\n    max-width: 15rem; }\n    .crud-header[data-v-e6d5254f] .crud-search[data-v-e6d5254f] .btn[data-v-e6d5254f] {\n      border-top-left-radius: 0;\n      border-bottom-left-radius: 0;\n      border-top-right-radius: 0.375rem;\n      border-bottom-right-radius: 0.375rem; }\n      .crud-header[data-v-e6d5254f] .crud-search[data-v-e6d5254f] .btn[data-v-e6d5254f].open[data-v-e6d5254f] {\n        border-top-right-radius: 0;\n        border-bottom-right-radius: 0; }\n  .crud-header[data-v-e6d5254f] .table-options[data-v-e6d5254f] {\n    margin-bottom: 1rem;\n    display: flex;\n    align-items: center;\n    justify-content: flex-end; }\n\n.custom-control[data-v-e6d5254f] {\n  position: relative; }\n\n@media (min-width: 992px) {\n  .table[data-v-e6d5254f] {\n    table-layout: auto; }\n    .table[data-v-e6d5254f] tbody[data-v-e6d5254f] td[data-v-e6d5254f] {\n      overflow: scroll;\n      -ms-overflow-style: none;\n      /* IE and Edge */\n      scrollbar-width: none;\n      /* Firefox */ }\n    .table[data-v-e6d5254f] tbody[data-v-e6d5254f] td[data-v-e6d5254f]::-webkit-scrollbar {\n      display: none; } }\n";
 n(css, {});
 
 function normalizeComponent (
@@ -12591,6 +12591,7 @@ const _sfc_main = {
         if (fieldName === primaryKey) {
           continue; // Salta este campo
         }
+        console.debug("debug field", field);
         if (field.type === 'relation') {
           // Si es una relación, inicializa como un objeto vacío.
 
@@ -12612,6 +12613,7 @@ const _sfc_main = {
                 break;
               // Agrega más casos según los tipos de campos que uses en tu modelo
               default:
+                console.debug("Undefined constructor ", fieldName, field.constructor);
                 // Tipo de campo no reconocido, puedes manejarlo de acuerdo a tus necesidades
                 itemDefault[fieldName] = null;
             }
@@ -14701,7 +14703,7 @@ var _sfc_render = function render() {
   }) : _vm._e()], 2) : _vm._e()], 1);
 };
 var _sfc_staticRenderFns = [];
-var __component__ = /*#__PURE__*/normalizeComponent(_sfc_main, _sfc_render, _sfc_staticRenderFns, false, null, "3ee88914", null, null);
+var __component__ = /*#__PURE__*/normalizeComponent(_sfc_main, _sfc_render, _sfc_staticRenderFns, false, null, "e6d5254f", null, null);
 var component = __component__.exports;
 
 // Import vue component
